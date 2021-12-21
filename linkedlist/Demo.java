a25ccaf7bb035f17c1a5a80bcc0db099bffd66d6
majjikishore007 <majjikishore031@gmail.com>
2021-12-21 10:10:29 +0530

    updated

--- linkedlist/Demo.java
+++ linkedlist/Demo.java
@@ -12,6 +12,7 @@ class Node{
 	//constructure of node 
 	public Node(int data){
 		this.data=data;	
+		this.next=null;
 	}
 
 }
@@ -21,7 +22,6 @@ class LinkedList{
 		adding a new node to the list
 	 */
 	public void add(int data){
-		
 		Node new_node=new Node(data);
 
 		if(head==null){
@@ -31,29 +31,46 @@ class LinkedList{
 		}
 		else {
 			Node temp=head;
-			System.out.println("not null"+temp.data);
+
 			while(temp.next!=null){
 				temp=temp.next;
 			}
 			temp.next=new_node;
-			new_node.next=null;
 		}
 	}
 	public void add(int data,int index){
 		Node new_node=new Node(data);
 		Node temp=head;
-		for(int i=0;i<index && temp.next!=null;i++){
-			temp=temp.next;
-			
+		if(index==0){
+			new_node.next=head;
+			head=new_node;
 		}
-		if(temp.next ==null){
-
-			temp.next = new_node;
-			new_node.next =null;
+		else {
+			while((index-1) !=0){
+				temp=temp.next;
+				index--;
+			}
+			new_node.next=temp.next;
+			temp.next=new_node;
 		}
-		else{
-			temp.next = new_node;
-			new_node.next = temp.next.next;
+	}
+	public void remove(int data){
+		Node temp=head;
+		if(temp.data==data){
+			head=head.next;
+			return;
+		}
+		while(temp.next!=null ){
+				if(temp.next.data==data){
+					break;
+				}
+				temp=temp.next;
+		}
+		if(temp.next.next!=null){
+			temp.next=temp.next.next;
+		}
+		else {
+			temp.next=null;
 		}
 	}	
 	public void print(){
@@ -68,14 +85,18 @@ class LinkedList{
 }
 public class Demo{
 	public static void main(String[] args) {
-		System.out.println("Starting ......");
+
 		LinkedList list=new LinkedList();
 
+		list.add(18);
+		list.add(10);
 		list.add(5);
 		list.add(3);
 		list.add(8);
-		list.add(7,1);
+		list.add(7);
 
+		list.remove(18);
+		list.remove(7);
 		list.print();
 	}
 }
