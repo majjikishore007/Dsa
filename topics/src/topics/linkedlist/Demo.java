cd1bfba96c0c4743f8c522cc56b4b000796c2107
majjikishore007 <majjikishore031@gmail.com>
2021-12-29 00:10:03 +0530

    updated

--- topics/src/topics/linkedlist/Demo.java
+++ topics/src/topics/linkedlist/Demo.java
@@ -6,6 +6,7 @@ package topics.linkedlist;
 class Node {
 	int data;
 	Node next;
+	Node random;
 
 	// constructure of node
 	public Node(int data) {
@@ -114,6 +115,17 @@ class LinkedList {
 		System.out.print("]");
 	}
 
+	public void print(Node head) {
+		Node temp = head;
+
+		System.out.print("[");
+		while (temp != null) {
+			System.out.print(temp.data + ",");
+			temp = temp.next;
+		}
+		System.out.print("]");
+	}
+
 	public void oddAndEven() {
 
 		Node oddhead = null, evenhead = null;
@@ -155,13 +167,116 @@ class LinkedList {
 		head = evenhead;
 
 	}
+
+	public Node reverse() {
+		Node prev = null, curr, fut;
+		curr = head;
+		while (curr.next != null) {
+			fut = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = fut;
+		}
+		curr.next = prev;
+		head = curr;
+
+		return head;
+
+	}
+
+	public Node reverse(Node start, Node end) {
+		Node prev = null, curr, fut;
+		curr = head;
+		while (curr.next != end) {
+			fut = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = fut;
+		}
+		curr.next = prev;
+		head = curr;
+
+		return head;
+
+	}
+
+	public Node reverse_kNodes(int k) {
+
+		if (k == 0 || k == 1) {
+			return head;
+		}
+		Node curr = head;
+		while (curr != null) {
+			if (has_kth_node(curr, k)) {
+
+				Node kth_node = get_kth_Node(curr, k);
+				// reverse
+
+				reverse(curr, kth_node);
+				// final head kth_node ,curr tail
+				curr.next = kth_node.next;
+
+			} else
+				curr = curr.next;
+		}
+		return head;
+
+	}
+
+	public boolean has_kth_node(Node curr, int k) {
+		if (curr.next == null) {
+			return false;
+		}
+		while (k > 0) {
+			curr = curr.next;
+			k--;
+		}
+		if (curr != null) {
+			return true;
+		}
+		return false;
+	}
+
+	public Node get_kth_Node(Node curr, int k) {
+
+		while (k > 0) {
+			curr = curr.next;
+			k--;
+		}
+		return curr;
+
+	}
+
+	public Node clonList() {
+		Node new_nodeh = new Node(head.data);
+		Node temp = head.next;
+		Node new_nodet;
+		new_nodet = new_nodeh;
+
+		while (temp != null) {
+			Node add = new Node(temp.data);
+			new_nodet.next = add;
+			new_nodet = new_nodet.next;
+			temp = temp.next;
+		}
+
+		Node temp1 = head;
+		Node new_temp = new_nodeh;
+		while (temp1 != null) {
+			Node ran = temp1.random;
+
+			new_temp.random = ran;
+			temp1 = temp1.next;
+		}
+
+		return new_nodeh;
+	}
 }
 
 public class Demo {
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
-
 		list.add(1);
 		list.add(2);
 		list.add(3);
@@ -169,11 +284,30 @@ public class Demo {
 		list.add(5);
 		list.add(6);
 		list.print();
+		LinkedList list1 = new LinkedList();
+		
+		list1.add(4);
+		list1.add(8);
+		list1.add(12);
+		list1.add(22);
+		list1.add(25);
+		list1.add(30);
+		list1.print();
+
+		Node head=Exaples.sortTWoList(list.head,list1.head);
+		list.print(head);
+		
+//		list.print();
 //		list.remove(18);
 //		list.remove(7);
 //		list.removeNthFromLast(4);
 //		list.removeNthFromLast(1);
-		list.oddAndEven();
-		list.print();
+//		list.oddAndEven();
+//		list.print();
+//		list.print();
+////		Node head_new = list.reverse();
+//		Node head = list.reverse_kNodes(2);
+//		list.print(head);
+
 	}
 }
