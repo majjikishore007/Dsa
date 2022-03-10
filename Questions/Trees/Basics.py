1da6742885e80783d8b4c582c1138ee675f4d066
majjikishore007 <majjikishore031@gmail.com>
2022-03-10 19:51:41 +0530

    updated 15

--- Questions/Trees/Basics.py
+++ Questions/Trees/Basics.py
@@ -1,59 +1,61 @@
 class Node:
     # constructure
-    def __init__(self,key):
-        self.data=key
-        self.left=None
-        self.right=None
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
 
+# curr , left , right
 
 
 def pre_order(root):
     if root:
-        print(root.data,end=" ")
+        print(root.data, end=" ")
         pre_order(root.left)
         pre_order(root.right)
+
+# left ,curr ,right
+
 
 def in_order(root):
     if root:
         in_order(root.left)
-        print(root.data,end=" ")
+        print(root.data, end=" ")
         in_order(root.right)
+
+# left ,right ,curr
+
 
 def post_order(root):
     if root:
         post_order(root.left)
         post_order(root.right)
-        print(root.data,end=" ")
+        print(root.data, end=" ")
 
-
-
-
-
+# this is level wise traversal
 
 
 def level_order(root):
     if root is None:
-        return 
+        return
 
     #creating a queue for storing all the elements
-    que=[]
-    
+    que = []
+
     que.add(root)
 
-    while(len(que)>0):
-        
+    while(len(que) > 0):
+
         print(que[0].data)
-        node =que.pop(0)
-        
-        #pushing the left element 
+        node = que.pop(0)
+
+        #pushing the left element
         if node.left is not None:
             que.append(node.left)
-        
+
         #pushing the right element
         if node.right is not None:
             que.append(node.right)
-
-
 
 
 root = Node(1)
@@ -64,6 +66,3 @@ root.left.right = Node(5)
 
 
 print(pre_order(root))
-
-        
-
---
+++ Questions/Trees/TreeTraversals.java
@@ -0,0 +1,8 @@
+'''
+The main travrsals methods are 
+- pre_order
+- post_order
+- in_order
+- level_order
+'''
+
