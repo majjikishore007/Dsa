25150ec2afea77697d2d4132b6e2e6e79a75c84d
majjikishore007 <majjikishore031@gmail.com>
2022-04-02 22:29:22 +0530

    question 1-mediumd

--- topics/src/basics/Sum.java
+++ topics/src/basics/Sum.java
@@ -4,23 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sum {
-	List<Integer> getPositiveCumulativeSum (int[] arr) {
-		// add your logic here
-		List<Integer>res=new ArrayList<>();
-		
-		int sum=0;
-		
-		for(int i=0;i<arr.length;i++) {
-			sum=sum+arr[i];
-			if(sum>=0) {
-				res.add(sum);
+	public static int getInt(int num) {
+		int ans=0;
+		int index=0;
+		while(num!=0) {
+			ans+=(num%10)*Math.pow(2,index++);
+			num=num/10;
+		}
+		return ans;
+	}
+	public  static boolean isPrime(int num) {
+		if(num%2==0) {
+			return false;
+		}
+		for(int i=3;i<=Math.sqrt(num);i++) {
+			if(num%i==0) {
+				return false;
 			}
 		}
-		
-		return res;
-		
+		return true;
 	}
+	public static  List<Integer>  getPrimeFromAtoB(int a,int  b) {
+	    List<Integer>ans=new ArrayList<Integer>();
+	    int  x = getInt(a);
+	    int  y = getInt(b);
+	
+	    for (int i = x; i <= y; i++) {
+	        if(isPrime(i)) {
+	        	ans.add(i);
+	        }
+	    }
+	    return ans;
+	}
+	
 	public static void main(String[] args) {
-
+		System.out.println(getPrimeFromAtoB(101,11111));
 	}
 }
