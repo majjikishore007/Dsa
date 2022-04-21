f278e398c9480bed8842b1c049bb23889cf04320
majjikishore007 <majjikishore031@gmail.com>
2022-04-21 19:52:24 +0530

    day 1-updated

--- Questions/Patterns/Question8.java
+++ Questions/Patterns/Question8.java
@@ -1,19 +1,9 @@
-
 public class Question8 {
     public static void print(int n) {
-        for (int i = 1; i < n; i++) {
-            // print spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print("\t");
-            }
-            for (int j = 0; j < i; j++) {
-                System.out.print("\t*\t");
-            }
-            System.out.println();
-        }
+
     }
 
     public static void main(String[] args) {
-        print(5);
+
     }
 }
