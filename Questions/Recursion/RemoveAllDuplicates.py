e7cd5de765b82bd1f84b19b36f0512bf67c3972a
majjikishore <majjikishore@localhost.localdomain>
2022-08-09 15:04:06 +0530

    starting from scracth

--- Questions/Recursion/RemoveAllDuplicates.py
+++
@@ -1,9 +0,0 @@
-def remove_duplictes(string, ans):
-    if len(string)-1 == 0:
-        ans+=string[0]
-        return ans
-    if string[0] != string[1]:
-        ans+=string[0]
-    return remove_duplictes(string[1:], ans)
-
-print(remove_duplictes("aaaaaa"," "))
